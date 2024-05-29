3f9e0a9f71a58b9349386364998f72e5efeff122
Sylvan Ravinet <dev@dlevel.net>
2024-05-29 19:10:46 +0200

    wip

--- cucumber.mjs
+++ cucumber.mjs
@@ -1,13 +1,21 @@
-import 'ts-node/register';
+//import 'ts-node/register';
 import { runCucumber } from '@cucumber/cucumber/api';
+import '@cucumber/pretty-formatter';
+import { DEFAULT_THEME } from '@cucumber/pretty-formatter';
 
 (async () => {
   const configuration = {
     require: ['tests/cucumber/step_definitions/**/*.ts'],
     paths: ['tests/cucumber/features/**/*.feature'],
-    format: ['@cucumber/pretty-formatter'], // Use the custom formatter
+    format: ['@cucumber/pretty-formatter'], // Ensure this is an array of strings
+    formatOptions: {
+      theme: {
+        ...DEFAULT_THEME,
+        'step text': 'magenta'
+      },
+    },
     support: {
-      requireModules: ['ts-node/register'],
+      //requireModules: ['ts-node/register'],
       require: ['tests/cucumber/step_definitions/**/*.ts', 'tests/cucumber/support/**/*.ts'],
     },
     formats: {
@@ -18,6 +26,8 @@ import { runCucumber } from '@cucumber/cucumber/api';
     },
     // Add any other configuration options you need here
   };
+
+  console.log('Configuration:', configuration); // Debugging line
 
   await runCucumber(configuration);
 })();
