090a82f8dd79bf479a6a86a02fbb87525c29bb07
Sylvan Ravinet <dev@dlevel.net>
2024-05-29 15:33:12 +0200

    wip

--- cucumber.mjs
+++ cucumber.mjs
@@ -1,9 +1,23 @@
-export default {
-  loader: ['ts-node/esm'],
-  import: [
-    'tests/cucumber/step_definitions/**/*.steps.ts',
-    'tests/cucumber/support/**/*.ts'
-  ],
-  paths: ['tests/cucumber/features/**/*.feature'],
-  format: ['progress'],
-}
+import 'ts-node/register';
+import { runCucumber } from '@cucumber/cucumber/api';
+
+(async () => {
+  const configuration = {
+    require: ['tests/cucumber/step_definitions/**/*.ts'],
+    paths: ['tests/cucumber/features/**/*.feature'],
+    format: ['@cucumber/pretty-formatter'], // Use the custom formatter
+    support: {
+      requireModules: ['ts-node/register'],
+      require: ['tests/cucumber/step_definitions/**/*.ts', 'tests/cucumber/support/**/*.ts'],
+    },
+    formats: {
+      publish: false, // Add this line to avoid the error
+    },
+    sources: {
+      paths: ['tests/cucumber/features/**/*.feature'], // Ensure paths are included in sources
+    },
+    // Add any other configuration options you need here
+  };
+
+  await runCucumber(configuration);
+})();
